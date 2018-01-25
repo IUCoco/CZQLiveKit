7fe2b82e0eb9f355c41efd480569f69bf0235221
Kikyo <czq1157111246@gmail.com>
2018-01-25 16:11:22 +0800

    0.1.0

---
+++ Example/Pods/Target Support Files/CZQLiveKit/CZQLiveKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CZQLiveKit : NSObject
+@end
+@implementation PodsDummy_CZQLiveKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-CZQLiveKit_Tests/Pods-CZQLiveKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CZQLiveKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CZQLiveKit_Tests
+@end
